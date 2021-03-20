d6ff4bb920ade1631acf61e3f0c39a5585ff143e
Durgaprasad S <durgaprasads4041999@gmail.com>
2021-03-20 20:46:43 +0530

    sign in form bloc and email,password value objects and validation

---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:fruvee/domain/authentication/auth_failure.dart';
+import 'package:fruvee/domain/authentication/i_auth_facade.dart';
+import 'package:fruvee/domain/authentication/value_objects.dart';
+import 'package:meta/meta.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    yield* event.map(emailChanged: (e) async* {
+      yield state.copyWith(
+        emailAddress: EmailAddress(e.emailStr),
+        authFailureSuccessOrFailureOption: none(),
+      );
+    }, passwordChanged: (e) async* {
+      yield state.copyWith(
+        password: Password(e.passwordStr),
+        authFailureSuccessOrFailureOption: none(),
+      );
+    }, registerWithEmailAndPasswordPressed: (e) async* {
+      yield* _performActionOnEmailAndPassword(
+        _authFacade.registerWithEmailAndPassword,
+      );
+    }, loginWithEmailAndPasswordPressed: (e) async* {
+      yield* _performActionOnEmailAndPassword(
+        _authFacade.signInWithEmailAndPassword,
+      );
+    }, signInWithGooglePressed: (e) async* {
+      yield state.copyWith(
+        isSubmitting: true,
+        authFailureSuccessOrFailureOption: none(),
+      );
+
+      final failureOrSuccess = await _authFacade.signInWithGoogle();
+
+      yield state.copyWith(
+        isSubmitting: false,
+        authFailureSuccessOrFailureOption: some(failureOrSuccess),
+      );
+    });
+  }
+
+  Stream<SignInFormState> _performActionOnEmailAndPassword(
+      Future<Either<AuthFailure, Unit>> Function(
+              {@required EmailAddress emailAddress,
+              @required Password password})
+          forwardedCall) async* {
+    final isEmailValid = state.emailAddress.isValid();
+    final isPasswordValid = state.password.isValid();
+
+    Either<AuthFailure, Unit> failureOrSuccess;
+
+    if (isEmailValid && isPasswordValid) {
+      yield state.copyWith(
+        isSubmitting: true,
+        authFailureSuccessOrFailureOption: none(),
+      );
+
+      failureOrSuccess = await forwardedCall(
+          emailAddress: state.emailAddress, password: state.password);
+    }
+
+    yield state.copyWith(
+      isSubmitting: false,
+      showErrorMessages: true,
+      authFailureSuccessOrFailureOption: some(failureOrSuccess),
+    );
+  }
+}
---
+++ lib/domain/authentication/i_auth_facade.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:fruvee/domain/authentication/auth_failure.dart';
+import 'package:fruvee/domain/authentication/value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+      {@required EmailAddress emailAddress, @required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
+      {@required EmailAddress emailAddress, @required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
